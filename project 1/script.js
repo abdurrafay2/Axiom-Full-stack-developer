2231144a43d0a0caee625cde2ada627282be4a09
Abdur Rafay <abdurrafay2@gmail.com>
2021-07-01 15:56:49 +0500

    changes for error
    created scripot

--- project 1/script.js
+++ project 1/script.js
@@ -3,18 +3,14 @@ const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const password2 = document.getElementById("paassword2");
+const confirmpassword = document.getElementById("confirmpassword");
 
 function showerror(input,message)
 {
     const formcontrol =input.parentElement;
-    formcontrol.className= 'form-control error'
+    formcontrol.className= 'form-control error' ;    
     const small = formcontrol.querySelector('small');
     small.innerText = message;
-
-
-
-
 
 }
 function showsuccess(input)
@@ -24,46 +20,67 @@ formcontrol.className='form-control success';
 
 }
 
+function checkRequired(inputArray)
+{
+    inputArray.forEach(function(input) {
+        if(input.value === '')
+        {
+            showerror(input,`${getFieldId(input)}  is required `);
+        }
+        else
+        {
+            showsuccess(input);
+        }
+        
+    });
+}
+function getFieldId(input)
+{
+return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 //create event for button
 form.addEventListener('submit', function(e) {
     e.preventDefault();
+    checkRequired([username,email,password,confirmpassword]);
+
     //console.log(username.value)
     //check if username empty
-    if(username.value ==='' )
-    {
-showerror(username, 'Username is required!')
+ //   if(username.value ==='' )
+    //{
+//showerror(username, 'Username is required!')
 
-    }
-    else {
-        showsuccess(username);
-    }
+  //  }
+   // else {
+     //   showsuccess(username);
+    //}
 
-    if(email.value ==='' )
-    {
-showerror(email, 'Email is required!')
+    //if(email.value ==='' )
+    //{
+//showerror(email, 'Email is required!')
 
-    }
-    else {
-        showsuccess(email);
-    }
+  //  }
+    //else {
+      //  showsuccess(email);
+   // }
 
-    if(password.value ==='' )
-    {
-showerror(password, 'Password is required!')
+    //if(password.value ==='' )
+    //{
+//showerror(password, 'Password is required!')
 
-    }
-    else {
-        showsuccess(password);
-    }
+  //  }
+    //else {
+      //  showsuccess(password);
+   // }
 
-    if(confirmpassword.value ==='' )
-    {
-showerror(confirmpassword, 'Confirm password is required!');
+    //if(confirmpassword.value ==='' )
+    //{
+//showerror(confirmpassword, 'Confirm password is required!');
 
-    }
-    else {
-        showsuccess(confirmpassword);
-    }
+  //  }
+    //else {
+      //  showsuccess(confirmpassword);
+    //}
     
 
     
